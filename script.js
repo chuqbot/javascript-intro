cfc5733df0a27c1f9bf8584bd01948d04bbba97e
Jeremy C <jchuq27@gmail.com>
2023-11-08 22:59:33 -0800

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+
+console.log("So far I am having a great time in professor Pascuzzi's class");
+var firstName = "Jeremy";
+var lastName = "Chuquimarca";
+var age = 19;
+alert("My name is " + firstName + lastName);
+alert("I am" + age);
